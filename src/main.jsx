11145bbdab467802bad4b53c37a50dc5cf946874
Linda Schönfeldt <linda.schonfeldt@gmail.com>
2025-10-22 23:56:27 +0200

    fix: replace Provider with ChakraProvider for correct context usage

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Provider } from '@chakra-ui/react'
+import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
 import { App } from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider>
+    <ChakraProvider value={defaultSystem}>
       <App />
-    </Provider>
+    </ChakraProvider>
   </React.StrictMode>
 )
