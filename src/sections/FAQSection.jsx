f93ac74f33d0672a360ffcba58a9327d741a5aaf
Linda Schönfeldt <linda.schonfeldt@gmail.com>
2025-10-29 12:19:03 +0100

    feat: update FAQSection to enhance contact link and add button for viewing all questions

--- src/sections/FAQSection.jsx
+++ src/sections/FAQSection.jsx
@@ -1,7 +1,9 @@
-import { Box, Container, Heading, Link, Text } from '@chakra-ui/react'
+import { Box, Button, Container, Heading, Link, Text } from '@chakra-ui/react'
+import { Link as RouterLink } from 'react-router-dom'
 
 import CustomAccordion from '../components/ui/CustomAccordion'
 import { selectedFaqs } from '../data/faqs'
+import { scrollToTop } from '../utils/navigation'
 
 const FAQSection = () => {
   return (
@@ -12,10 +14,23 @@ const FAQSection = () => {
         </Heading>
         <Text fontSize='lg' color='gray.600' mb={8} maxW='2xl' mx='auto'>
           Här finner ni svar på våra vanligaste frågor. Har ni ytterligare
-          funderingar är ni alltid välkomna att{' '}
-          <Link href='#contact'>kontakta oss</Link>.
+          funderingar är ni alltid välkomna att
+          <Link as={RouterLink} to='/kontakt' color='blue.600'>
+            kontakta oss
+          </Link>
+          .
         </Text>
         <CustomAccordion faqs={selectedFaqs} />
+        <Button
+          as={RouterLink}
+          to='/vanliga-fragor'
+          variant='outline'
+          colorScheme='blue'
+          mt={4}
+          onClick={scrollToTop}
+        >
+          Visa alla frågor
+        </Button>
       </Container>
     </Box>
   )
