4f4af58a0b5705581f1f32b9038f0cc1905a7a56
Linda Schönfeldt <linda.schonfeldt@gmail.com>
2025-11-07 13:29:52 +0100

    feat(FAQ): enhance FAQ page with accordion component and contact link

--- src/pages/FAQ.tsx
+++ src/pages/FAQ.tsx
@@ -1,4 +1,6 @@
-import { Box, Heading } from '@chakra-ui/react'
+import { Box, Heading, Text } from '@chakra-ui/react'
+import CustomAccordion from '../components/ui/CustomAccordion'
+import { faqs } from '../data/faqs'
 
 const FAQ = () => {
   return (
@@ -12,6 +14,25 @@ const FAQ = () => {
       <Heading as='h1' size='xl' pt={8} ml={4} mb={4}>
         Vanliga frågor (FAQ)
       </Heading>
+      <Box
+        maxW={{ base: '100%', md: 'container.md', lg: 'container.lg' }}
+        mx='auto'
+        px={{ base: 4, md: 8 }}
+        pb={16}
+      >
+        <CustomAccordion faqs={faqs} />
+
+        <Text mt={8} fontSize='md' color='gray.600'>
+          Har du fler frågor? Tveka inte att{' '}
+          <a
+            href='/contact'
+            style={{ color: '#3182CE', textDecoration: 'underline' }}
+          >
+            kontakta oss
+          </a>
+          !
+        </Text>
+      </Box>
     </Box>
   )
 }
