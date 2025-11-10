4282472807c47bdeb50a0018d1c802a0823e391e
Linda Schönfeldt <linda.schonfeldt@gmail.com>
2025-11-10 13:16:40 +0100

    feat(FAQ): enhance FAQ page structure with improved meta tags and layout

--- src/pages/FAQ.tsx
+++ src/pages/FAQ.tsx
@@ -1,37 +1,42 @@
 import { Box, Text } from '@chakra-ui/react'
 
+import PageSection from '../components/layout/PageSection'
 import PageWrapper from '../components/layout/PageWrapper'
+import Meta from '../components/Meta'
 import IntroSection from '../components/sections/IntroSection'
 import CustomAccordion from '../components/ui/CustomAccordion'
 import { faqs } from '../data/faqs'
 
 const FAQ = () => {
   return (
-    <PageWrapper>
-      <IntroSection
-        heading='Vanliga frågor'
-        introText='Här hittar du svar på de vanligaste frågorna om våra bokföringstjänster, priser och processer.'
+    <>
+      <Meta
+        title='Vanliga frågor - Billig Bokföring'
+        description='Här hittar du svar på de vanligaste frågorna om våra tjänster, priser och processer.'
+        keywords='vanliga frågor bokföring, FAQ bokföringsbyrå, bokföringstjänster frågor, priser bokföring'
+        ogUrl='https://billigbokforing.se/faq'
       />
-      <Box
-        maxW='container.xl'
-        mx='auto'
-        textAlign='left'
-        m={{ base: 4, md: 8, lg: 16 }}
-      >
-        <CustomAccordion faqs={faqs} />
+      <PageWrapper>
+        <IntroSection
+          heading='Vanliga frågor'
+          introText='Här hittar du svar på de vanligaste frågorna om våra bokföringstjänster, priser och processer.'
+        />
+        <PageSection>
+          <CustomAccordion faqs={faqs} />
 
-        <Text mt={8} fontSize='md' color='gray.600' textAlign='center'>
-          Har du fler frågor? Tveka inte att{' '}
-          <a
-            href='/kontakt'
-            style={{ color: '#3182CE', textDecoration: 'underline' }}
-          >
-            kontakta oss
-          </a>
-          !
-        </Text>
-      </Box>
-    </PageWrapper>
+          <Text mt={8} fontSize='md' color='gray.600' textAlign='center'>
+            Har du fler frågor? Tveka inte att{' '}
+            <a
+              href='/kontakt'
+              style={{ color: '#3182CE', textDecoration: 'underline' }}
+            >
+              kontakta oss
+            </a>
+            !
+          </Text>
+        </PageSection>
+      </PageWrapper>
+    </>
   )
 }
 
